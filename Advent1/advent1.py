677499450292ea3788eb4e3f135937d7884b8ce9
Bel-Pepper <42isall@gmail.com>
2022-12-01 14:32:31 -0500

    Now with comments
    Comments

--- Advent1/advent1.py
+++ Advent1/advent1.py
@@ -1,17 +1,25 @@
+#Solution for Advent of Code Challenge 1 part 1 in python
+#Still have comments for when using the test document and actually wanting to print everything out
 
 #Put your file path here!!!
-fileLocation = ThisSpaceIntentionallyLeftBlank
+fileLocation = "ThisSpaceIntentionallyLeftBlank"
 
 file = open(fileLocation)
+#Lets initialize some shit
 elf = 1
 total = 0
 high = 0
+
+#The main loop. Tell me you are used to C++ without telling me you are used to C++
 while True:
     next_Line = file.readline()
 
+#   Check to see if the current elf is done reporting calories
     if next_Line.strip() != '':
         total = int(next_Line.strip()) + total
         
+    #if they are then sum up the total and check to see if it beats the high score
+    
     else:
         #print ("total = ", total)
         if total > high:
@@ -19,7 +27,7 @@ while True:
             #print ("New high is ", high)
         total = 0
 
-
+    #At the end of the file print the high score and close the loop
     if not next_Line:
         print ("Highest total is", high)
         break;
