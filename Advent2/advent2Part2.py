fd16852b8ab9e2b042065aeae4f6f06b993cac62
Bel-Pepper <42isall@gmail.com>
2022-12-02 02:04:36 -0500

    Advent day 2 part 2

--- Advent2/advent2Part2.py
+++ Advent2/advent2Part2.py
@@ -2,7 +2,6 @@
 
 #Put your file path here!!!
 fileLocation = "ThisSpaceIntentionallyLeftBlank"
-
 file = open(fileLocation)
 
 #initialize some variables
@@ -10,6 +9,7 @@ oppChoice = 0
 plaChoice = 0
 plaTotal = 0
 scoreTotal = 0
+resNeeded = 0
 
 #Check to see if you won or lost. Your moves are x, opponents are y
 def WinLose(x,y):
@@ -42,6 +42,7 @@ def resScore(x):
     elif x == "Draw":
         score = 3
     return score
+
 #gets the score from what you played
 #I don't think this is how people actually play RPS
 #Elves are fucking weird
@@ -53,12 +54,36 @@ def playScore(x):
     elif x == "S":
         score=3
     return score
+#Tells you what you need to choose
+#based on what the desired outcome is (x) and what the opponent chose (y)
+def whatDo(x,y):
+    if x == "W":
+        if y=="R":
+            do = "P"
+        elif y=="P":
+            do = "S"
+        elif y== "S":
+            do = "R"
+    if x == "L":
+        if y=="R":
+            do = "S"
+        elif y=="P":
+            do = "R"
+        elif y== "S":
+            do = "P"
+
+    if x == "D":
+        do=y
+    return do
+
+
 
 while True:
 
     next_Line=(file.readline())
     
     print (next_Line)
+#Takes in what you did and what you needed do
     if next_Line:
         if "A" in next_Line:
             #print ("Opponent chose Rock")
@@ -72,13 +97,19 @@ while True:
             
         if "X" in next_Line:
             #print ("You chose Rock")
-            plaChoice="R"
+            resNeeded="L"
         elif "Y" in next_Line:
             #print ("You chose Paper")
-            plaChoice="P"
+            resNeeded="D"
         elif "Z" in next_Line:
             #print ("You chose Scissors")
-            plaChoice="S"
+            resNeeded="W"
+#Now calls all the things that actually do the things
+
+#Tells you what to do, saves it to plaChoice
+        plaChoice = whatDo(resNeeded, oppChoice)
+
+#Now to calculate score, and then add to the
         result = WinLose(plaChoice, oppChoice)
         #print ("Your result:", result)
         score = resScore(result)+playScore(plaChoice)
