460fa02c2648efb2e5eff353198d036aa1309610
Bel-Pepper <42isall@gmail.com>
2022-12-08 14:55:12 -0500

    General maintenance

---
+++ Advent0/advent0.py
@@ -0,0 +1,19 @@
+#This is just the base format for me to copy in cause I'm laaazy
+# Sorry, "Efficient"
+
+#Advent of Code Challenge n part  in python
+
+from pathlib import Path
+file_to_open=Path("Advent5/advent5TestStart.txt")
+
+
+
+def main():
+
+    for line in f:
+        dostuff
+
+        
+if __name__ == "__main__":
+    main()
+    f.close
---
+++ Advent6/advent6.py
@@ -0,0 +1,17 @@
+
+#Advent of Code Challenge n part  in python
+
+from pathlib import Path
+file_to_open=Path("Advent5/advent5TestStart.txt")
+
+
+
+def main():
+
+    for line in f:
+        dostuff
+
+        
+if __name__ == "__main__":
+    main()
+    f.close
